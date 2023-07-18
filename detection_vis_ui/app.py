39f1db1a4d688b137c6a56cb3087d51220b64a9f
kathy <kangle.leehd@gmail.com>
2023-07-18 12:10:39 +0200

    move data choosing into a separate streamlit page

--- detection_vis_ui/app.py
+++ detection_vis_ui/app.py
@@ -1,8 +1,5 @@
-import requests
-import os
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-import pandas as pd
+
 
 st.set_page_config(
     page_title="Have fun ",
@@ -12,122 +9,8 @@ st.set_page_config(
 # Define the title 
 st.title("Automotive sensor data visualization web application")
 
-backend_service = os.getenv('BACKEND_SERVICE', 'localhost')
+st.write("This project demonstrates ... into an interactive Streamlit app.")
 
-# ########################
-# response = requests.get("http://detection_vis_backend:8001/users")  # or your FastAPI server URL
-# users = response.json()
-# st.json(users)
-# ########################
-
-response = requests.get(f"http://{backend_service}:8001/datasets")  # or your FastAPI server URL
-datasets = response.json()
-
-# show datasets on top level
-root_datasets = []
-for d in datasets:
-  if d["parent_id"] is None:
-    root_datasets.append(d)
-dataset_name = st.selectbox("Choose the dataset", [i["name"] for i in root_datasets])
-st.subheader(f"{dataset_name} Dataset")
-
-# get the chosen dataset
-dataset = {}
-for d in datasets:
-  if d["name"] == dataset_name:
-    dataset = d
-    st.write(d["description"])
-
-# get subdatasets from the chosen dataset
-subdatasets = []
-for d in datasets:
-  if d["parent_id"] == dataset["id"]:
-    subdatasets.append(d)
-
-if 'datafiles_chosen' not in st.session_state:
-  st.session_state.datafiles_chosen = []
-
-if subdatasets:
-  # show subdatasets in tab pages
-  subdataset_tabs = st.tabs([i["name"] for i in subdatasets])
-  index = 0
-  for i, t in enumerate(subdataset_tabs):
-    with t:
-      # Show data files 
-      response = requests.get(f"http://{backend_service}:8001/dataset/{subdatasets[index]['id']}")
-      files = response.json()
-
-      colms = st.columns((1, 2, 2, 1, 1))
-      fields = ['No', 'File name', 'info', "size", 'select']
-      # table header
-      for col, field_name in zip(colms, fields):
-        col.write(field_name)
-      # table content
-      for no, file in enumerate(files):
-        col1, col2, col3, col4, col5 = st.columns((1, 2, 2, 1, 1))
-        col1.write(no)  
-        col2.write(file["name"])  
-        col3.write(file["description"])  
-        col4.write(file["size"]) 
-        # last column
-        checkbox = f"{dataset['name']}_{index}_{file['name']}"
-        if  checkbox not in st.session_state:
-          checkbox_status = col5.checkbox("", value=False,key=f"checkbox_{checkbox}")
-          st.session_state[checkbox] = False
-        elif st.session_state[checkbox]:
-          checkbox_status = col5.checkbox("", value=True,key=f"checkbox_{checkbox}")
-        else:
-          checkbox_status = col5.checkbox("", value=False,key=f"checkbox_{checkbox}")
-
-        if checkbox_status and file not in st.session_state.datafiles_chosen:
-          st.session_state.datafiles_chosen.append(file)
-          st.session_state[checkbox] = True
-
-        if not checkbox_status and file in st.session_state.datafiles_chosen:
-          st.session_state[checkbox] = False
-          st.session_state.datafiles_chosen.remove(file)
-
-    index = index + 1
-else:
-  # Show data files 
-  response = requests.get(f"http://{backend_service}:8001/dataset/{dataset['id']}")
-  files = response.json()
-
-  colms = st.columns((1, 2, 2, 1, 1))
-  fields = ['No', 'File name', 'info', "size", 'select']
-  # table header
-  for col, field_name in zip(colms, fields):
-    col.write(field_name)
-  # table content
-  for no, file in enumerate(files):
-    col1, col2, col3, col4, col5 = st.columns((1, 2, 2, 1, 1))
-    col1.write(no)  
-    col2.write(file["name"])  
-    col3.write(file["description"])  
-    col4.write(file["size"]) 
-    # last column
-    checkbox = f"{dataset['name']}_{file['name']}"
-    if checkbox not in st.session_state:
-      checkbox_status = col5.checkbox("", value=False,key=f"checkbox_{checkbox}")
-      st.session_state[f"{dataset['name']}_{file['name']}"] = False
-    elif st.session_state[f"{dataset['name']}_{file['name']}"]:
-      checkbox_status = col5.checkbox("", value=True,key=f"checkbox_{checkbox}")
-    else:
-      checkbox_status = col5.checkbox("", value=False,key=f"checkbox_{checkbox}")
-
-    if checkbox_status and file not in st.session_state.datafiles_chosen:
-      st.session_state.datafiles_chosen.append(file)
-      st.session_state[f"{dataset['name']}_{file['name']}"] = True
-
-    if not checkbox_status and file in st.session_state.datafiles_chosen:
-      st.session_state[f"{dataset['name']}_{file['name']}"] = False
-      st.session_state.datafiles_chosen.remove(file)
-    
-
-st.write(st.session_state.datafiles_chosen)
-button_click = st.button("Check feature")
-if button_click:
-  #check_datafiles(st.session_state.datafiles_chosen)
-  switch_page("get features")
+st.write("👈 Please select Choose data in the sidebar to start.")
 
   
--- detection_vis_ui/pages/1_Choose_data.py
+++ detection_vis_ui/pages/1_Choose_data.py
@@ -1,6 +1,12 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
+import requests
+import os
+
+from streamlit_extras.switch_page_button import switch_page
+
 
 
 st.set_page_config(
@@ -8,11 +14,132 @@ st.set_page_config(
     page_icon=":red_car:", 
 )
 
-import requests
-import os
-import time
-import numpy as np
-import matplotlib.pyplot as plt
+
+backend_service = os.getenv('BACKEND_SERVICE', 'localhost')
+
+# ########################
+# response = requests.get("http://detection_vis_backend:8001/users")  # or your FastAPI server URL
+# users = response.json()
+# st.json(users)
+# ########################
+
+response = requests.get(f"http://{backend_service}:8001/datasets")  # or your FastAPI server URL
+datasets = response.json()
+
+# show datasets on top level
+root_datasets = []
+for d in datasets:
+  if d["parent_id"] is None:
+    root_datasets.append(d)
+dataset_name = st.selectbox("Choose a dataset", [i["name"] for i in root_datasets])
+st.subheader(f"{dataset_name} Dataset")
+
+# get the chosen dataset
+dataset = {}
+for d in datasets:
+  if d["name"] == dataset_name:
+    dataset = d
+    st.write(d["description"])
+
+# get subdatasets from the chosen dataset
+subdatasets = []
+for d in datasets:
+  if d["parent_id"] == dataset["id"]:
+    subdatasets.append(d)
+
+if 'datafiles_chosen' not in st.session_state:
+  st.session_state.datafiles_chosen = []
+
+if subdatasets:
+  # show subdatasets in tab pages
+  subdataset_tabs = st.tabs([i["name"] for i in subdatasets])
+  index = 0
+  for i, t in enumerate(subdataset_tabs):
+    with t:
+      # Show data files 
+      response = requests.get(f"http://{backend_service}:8001/dataset/{subdatasets[index]['id']}")
+      files = response.json()
+
+      colms = st.columns((1, 2, 2, 1, 1))
+      fields = ['No', 'File name', 'info', "size", 'select']
+      # table header
+      for col, field_name in zip(colms, fields):
+        col.write(field_name)
+      # table content
+      for no, file in enumerate(files):
+        col1, col2, col3, col4, col5 = st.columns((1, 2, 2, 1, 1))
+        col1.write(no)  
+        col2.write(file["name"])  
+        col3.write(file["description"])  
+        col4.write(file["size"]) 
+        # last column
+        checkbox = f"{dataset['name']}_{index}_{file['name']}"
+        if  checkbox not in st.session_state:
+          checkbox_status = col5.checkbox(" ", value=False,key=f"checkbox_{checkbox}")
+          st.session_state[checkbox] = False
+        elif st.session_state[checkbox]:
+          checkbox_status = col5.checkbox(" ", value=True,key=f"checkbox_{checkbox}")
+        else:
+          checkbox_status = col5.checkbox(" ", value=False,key=f"checkbox_{checkbox}")
+
+        if checkbox_status and file not in st.session_state.datafiles_chosen:
+          st.session_state.datafiles_chosen.append(file)
+          st.session_state[checkbox] = True
+
+        if not checkbox_status and file in st.session_state.datafiles_chosen:
+          st.session_state[checkbox] = False
+          st.session_state.datafiles_chosen.remove(file)
+
+    index = index + 1
+else:
+  # Show data files 
+  response = requests.get(f"http://{backend_service}:8001/dataset/{dataset['id']}")
+  files = response.json()
+
+  colms = st.columns((1, 2, 2, 1, 1))
+  fields = ['No', 'File name', 'info', "size", 'select']
+  # table header
+  for col, field_name in zip(colms, fields):
+    col.write(field_name)
+  # table content
+  for no, file in enumerate(files):
+    col1, col2, col3, col4, col5 = st.columns((1, 2, 2, 1, 1))
+    col1.write(no)  
+    col2.write(file["name"])  
+    col3.write(file["description"])  
+    col4.write(file["size"]) 
+    # last column
+    checkbox = f"{dataset['name']}_{file['name']}"
+    if checkbox not in st.session_state:
+      checkbox_status = col5.checkbox(" ", value=False,key=f"checkbox_{checkbox}")
+      st.session_state[f"{dataset['name']}_{file['name']}"] = False
+    elif st.session_state[f"{dataset['name']}_{file['name']}"]:
+      checkbox_status = col5.checkbox(" ", value=True,key=f"checkbox_{checkbox}")
+    else:
+      checkbox_status = col5.checkbox(" ", value=False,key=f"checkbox_{checkbox}")
+
+    if checkbox_status and file not in st.session_state.datafiles_chosen:
+      st.session_state.datafiles_chosen.append(file)
+      st.session_state[f"{dataset['name']}_{file['name']}"] = True
+
+    if not checkbox_status and file in st.session_state.datafiles_chosen:
+      st.session_state[f"{dataset['name']}_{file['name']}"] = False
+      st.session_state.datafiles_chosen.remove(file)
+    
+
+# st.write(st.session_state.datafiles_chosen)
+button_click = st.button("Check feature")
+if button_click:
+  #check_datafiles(st.session_state.datafiles_chosen)
+  switch_page("get features")
+
+
+################################################
+# import requests
+# import os
+# import time
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 # complex_array = np.random.rand(32, 8, 304) + 1j * np.random.rand(32, 8, 304)
 # print(complex_array[0,0,0])
@@ -38,69 +165,6 @@ import matplotlib.pyplot as plt
 ##############################################################
 
 
-def show_next(i, length):
-  # Increments the counter to get next photo
-  st.session_state[i] += 1
-  if st.session_state[i] >= length:
-    st.session_state[i] = 0
-  
-
-def show_last(i, length):
-  # Decrements the counter to get next photo
-  st.session_state[i] -= 1
-  if st.session_state[i] < 0:
-    st.session_state[i] = length-1
-
-
-features = ["RAD", "RA", "RD", "AD", "spectrogram", "radarPC", "lidarPC", "image", "depth_image"]
-show_status = [False, False, False, False, False, False, False, True, False]
-# features = ["RAD", "image", "lidar"]
-# show_status = [False, False, True]
-
-expanders = [None]*len(features)
-counters = [f"counter_{f}" for f in features]
-placeholders = [[None]*6 for _ in range(len(features)) ]
-load_actions = [None]*len(features)
-
-# test expander with button 
-for idx, (counter, feature) in enumerate(zip(counters, features)):
-  expanders[idx] = st.expander(feature, expanded=True)
-  with expanders[idx]:
-    if counter not in st.session_state: 
-      st.session_state[counter] = 0
-    if feature not in st.session_state:
-      st.session_state[feature] = show_status[idx]
-
-    if st.session_state[feature]:
-
-      fileset = [os.path.join(feature,f) for f in os.listdir(feature)]
-      st.write(f"Total frames: {len(fileset)}")
-      forward_btn = st.button("Show next frame ⏭️",on_click=show_next,args=([counter,len(fileset)]), key=f"{feature}_forward_btn")
-      backward_btn = st.button("Show last frame ⏪",on_click=show_last,args=([counter,len(fileset)]), key=f"{feature}_backward_btn")
-      photo = fileset[st.session_state[counter]]
-      st.image(photo,caption=photo)
-      st.write(f"Index : {st.session_state[counter]}")
-    else:
-
-      for i in range(len(placeholders[idx])):
-        placeholders[idx][i] = st.empty()
-
-      load_actions[idx] = placeholders[idx][0].button("Get feature", key=f"{feature}_load_btn")
-      if load_actions[idx]:
-        placeholders[idx][0].empty()
-        st.session_state[feature] = True
-        with expanders[idx]:
-          fileset = [os.path.join(feature,f) for f in os.listdir(feature)]
-          #placeholder1.empty()
-          placeholders[idx][1].write(f"Total frames: {len(fileset)}")
-          forward_btn = placeholders[idx][2].button("Show next frame ⏭️",on_click=show_next,args=([counter,len(fileset)]), key=f"{feature}_forward_btn")
-          backward_btn = placeholders[idx][3].button("Show last frame ⏪",on_click=show_last,args=([counter,len(fileset)]), key=f"{feature}_backward_btn")
-          photo = fileset[st.session_state[counter]]
-          placeholders[idx][4].image(photo,caption=photo)
-          placeholders[idx][5].write(f"Index : {st.session_state[counter]}")
-            
-
-#st.info(f"load state: {st.session_state['load_state']}")
 
 ########################################################################
 # Don't change!
