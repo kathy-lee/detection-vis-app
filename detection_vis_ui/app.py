9a9dc7094e748fe9ec0617deb776f939359eec08
kathy <kangle.leehd@gmail.com>
2023-07-24 22:26:11 +0200

    update intro page

--- detection_vis_ui/app.py
+++ detection_vis_ui/app.py
@@ -9,8 +9,13 @@ st.set_page_config(
 # Define the title 
 st.title("Automotive sensor data visualization web application")
 
-st.write("This project demonstrates ... into an interactive Streamlit app.")
+st.write("This project demonstrates the following features into an interactive Streamlit app.")
 
-st.write("👈 Please select Choose data in the sidebar to start.")
+st.write("1. Visualize data & features of open source automotive datasets.")
+st.write("2. Train object detection model with selected data & feature & model & train configs from supported model types and persist the model.")
+st.write("3. Object detection inference with persisted models.")
+
+st.write("👈 Please select Choose data in the sidebar to start if you want 1 or 2.")
+st.write("👈 Please select Inference in the sidebar to start if you want 3.")
 
   
