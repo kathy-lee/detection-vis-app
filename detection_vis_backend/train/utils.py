f6b365319cbff96440ab542912333cc281055a58
kathy <kangle.leehd@gmail.com>
2023-07-27 22:16:39 +0200

    add train util functions

--- detection_vis_backend/train/utils.py
+++ detection_vis_backend/train/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
-from torch.utils.data import Dataset, DataLoader, random_split,Subset
-import numpy as np
 import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+from torch.utils.data import Dataset, DataLoader, random_split, Subset
+
 
 
 # class TrainDataset(Dataset):
@@ -141,3 +144,135 @@ def RADIal_collate(batch):
         images.append(torch.tensor(image))
         labels.append(torch.from_numpy(box_labels))    
     return torch.stack(FFTs), torch.stack(encoded_label),torch.stack(segmaps),labels,torch.stack(images)
+
+
+def run_evaluation(net,loader,encoder,check_perf=False, detection_loss=None,segmentation_loss=None,losses_params=None):
+
+    metrics = Metrics()
+    metrics.reset()
+
+    net.eval()
+    running_loss = 0.0
+    
+    kbar = pkbar.Kbar(target=len(loader), width=20, always_stateful=False)
+
+    for i, data in enumerate(loader):
+
+        # input, out_label,segmap,labels
+        inputs = data[0].to('cuda').float()
+        label_map = data[1].to('cuda').float()
+        seg_map_label = data[2].to('cuda').double()
+
+        with torch.set_grad_enabled(False):
+            outputs = net(inputs)
+
+        if(detection_loss!=None and segmentation_loss!=None):
+            classif_loss,reg_loss = detection_loss(outputs['Detection'], label_map,losses_params)           
+            prediction = outputs['Segmentation'].contiguous().flatten()
+            label = seg_map_label.contiguous().flatten()        
+            loss_seg = segmentation_loss(prediction, label)
+            loss_seg *= inputs.size(0)
+                
+
+            classif_loss *= losses_params['weight'][0]
+            reg_loss *= losses_params['weight'][1]
+            loss_seg *=losses_params['weight'][2]
+
+
+            loss = classif_loss + reg_loss + loss_seg
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+
+        if(check_perf):
+            out_obj = outputs['Detection'].detach().cpu().numpy().copy()
+            labels = data[3]
+
+            out_seg = torch.sigmoid(outputs['Segmentation']).detach().cpu().numpy().copy()
+            label_freespace = seg_map_label.detach().cpu().numpy().copy()
+
+            for pred_obj,pred_map,true_obj,true_map in zip(out_obj,out_seg,labels,label_freespace):
+
+                metrics.update(pred_map[0],true_map,np.asarray(encoder.decode(pred_obj,0.05)),true_obj,
+                            threshold=0.2,range_min=5,range_max=100) 
+                
+        kbar.update(i)
+        
+    mAP,mAR, mIoU = metrics.GetMetrics()
+
+    return {'loss':running_loss, 'mAP':mAP, 'mAR':mAR, 'mIoU':mIoU}
+
+
+
+class FocalLoss(nn.Module):
+    """
+    Focal loss class. Stabilize training by reducing the weight of easily classified background sample and focussing
+    on difficult foreground detections.
+    """
+
+    def __init__(self, gamma=0, size_average=False):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.size_average = size_average
+
+    def forward(self, prediction, target):
+
+        # get class probability
+        pt = torch.where(target == 1.0, prediction, 1-prediction)
+
+        # compute focal loss
+        loss = -1 * (1-pt)**self.gamma * torch.log(pt+1e-6)
+
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
+
+
+
+def pixor_loss(batch_predictions, batch_labels,param):
+
+    #########################
+    #  classification loss  #
+    #########################
+    classification_prediction = batch_predictions[:, 0,:, :].contiguous().flatten()
+    classification_label = batch_labels[:, 0,:, :].contiguous().flatten()
+
+    if(param['classification']=='FocalLoss'):
+        focal_loss = FocalLoss(gamma=2)
+        classification_loss = focal_loss(classification_prediction, classification_label)
+    else:
+        classification_loss = F.binary_cross_entropy(classification_prediction.double(), classification_label.double(),reduction='sum')
+
+    
+    #####################
+    #  Regression loss  #
+    #####################
+
+    regression_prediction = batch_predictions.permute([0, 2, 3, 1])[:, :, :, :-1]
+    regression_prediction = regression_prediction.contiguous().view([regression_prediction.size(0)*
+                        regression_prediction.size(1)*regression_prediction.size(2), regression_prediction.size(3)])
+    regression_label = batch_labels.permute([0, 2, 3, 1])[:, :, :, :-1]
+    regression_label = regression_label.contiguous().view([regression_label.size(0)*regression_label.size(1)*
+                                                           regression_label.size(2), regression_label.size(3)])
+
+    positive_mask = torch.nonzero(torch.sum(torch.abs(regression_label), dim=1))
+    pos_regression_label = regression_label[positive_mask.squeeze(), :]
+    pos_regression_prediction = regression_prediction[positive_mask.squeeze(), :]
+
+
+    T = batch_labels[:,1:]
+    P = batch_predictions[:,1:]
+    M = batch_labels[:,0].unsqueeze(1)
+
+    if(param['regression']=='SmoothL1Loss'):
+        reg_loss_fct = nn.SmoothL1Loss(reduction='sum')
+    else:
+        reg_loss_fct = nn.L1Loss(reduction='sum')
+    
+    regression_loss = reg_loss_fct(P*M,T)
+    NbPts = M.sum()
+    if(NbPts>0):
+        regression_loss/=NbPts
+
+    return classification_loss,regression_loss
