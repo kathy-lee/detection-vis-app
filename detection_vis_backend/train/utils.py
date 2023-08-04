ed31f8d231f5b9da62a4b1d7c1b4c874913b39e7
kathy <kangle.leehd@gmail.com>
2023-08-04 20:01:48 +0200

    add run_FullEvaluation() method

--- detection_vis_backend/train/utils.py
+++ detection_vis_backend/train/utils.py
@@ -58,20 +58,23 @@ def CreateDataLoaders(dataset,config=None,seed=0):
                                 num_workers=config['train']['num_workers'],
                                 pin_memory=True,
                                 collate_fn=RADIal_collate)
-        val_loader =  DataLoader(val_dataset, 
+        val_loader = DataLoader(val_dataset, 
                                 batch_size=config['val']['batch_size'], 
                                 shuffle=False,
                                 num_workers=config['val']['num_workers'],
                                 pin_memory=True,
                                 collate_fn=RADIal_collate)
-        test_loader =  DataLoader(test_dataset, 
+        test_loader = DataLoader(test_dataset, 
                                 batch_size=config['test']['batch_size'], 
                                 shuffle=False,
                                 num_workers=config['test']['num_workers'],
                                 pin_memory=True,
                                 collate_fn=RADIal_collate)
 
-        return train_loader,val_loader,test_loader
+        train_ids = train_dataset.indices
+        val_ids = val_dataset.indices
+        test_ids = test_dataset.indices
+        return train_loader,val_loader,test_loader,train_ids,val_ids,test_ids
     elif(config['mode']=='sequence'):
         Sequences = {'Validation':['RECORD@2020-11-22_12.49.56', 'RECORD@2020-11-22_12.11.49',
                                    'RECORD@2020-11-22_12.28.47','RECORD@2020-11-21_14.25.06'],
@@ -137,7 +140,7 @@ def CreateDataLoaders(dataset,config=None,seed=0):
                                 pin_memory=True,
                                 collate_fn=RADIal_collate)
 
-        return train_loader,val_loader,test_loader
+        return train_loader,val_loader,test_loader,train_ids,val_ids,test_ids
         
     else:      
         raise NameError(config['mode'], 'is not supported !')
@@ -257,6 +260,60 @@ def run_evaluation(net,loader,check_perf=False, detection_loss=None,segmentation
     return {'loss':running_loss, 'mAP':mAP, 'mAR':mAR, 'mIoU':mIoU}
 
 
+def run_FullEvaluation(net, loader, eval_path, iou_threshold=0.5):
+
+    net.eval()
+    
+    kbar = pkbar.Kbar(target=len(loader), width=20, always_stateful=False)
+
+    print('Generating Predictions...')
+    predictions = {'prediction':{'objects':[],'freespace':[]},'label':{'objects':[],'freespace':[]}}
+    for i, data in enumerate(loader):
+
+        # input, out_label,segmap,labels
+        inputs = data[0].to('cuda').float()
+
+        with torch.set_grad_enabled(False):
+            outputs = net(inputs)
+
+        out_obj = outputs['Detection'].detach().cpu().numpy().copy()
+        out_seg = torch.sigmoid(outputs['Segmentation']).detach().cpu().numpy().copy()
+        
+        labels_object = data[3]
+        label_freespace = data[2].numpy().copy()
+            
+        for pred_obj,pred_map,true_obj,true_map in zip(out_obj,out_seg,labels_object,label_freespace):
+            
+            predictions['prediction']['objects'].append( np.asarray(decode(pred_obj,0.05)))
+            predictions['label']['objects'].append(true_obj)
+
+            predictions['prediction']['freespace'].append(pred_map[0])
+            predictions['label']['freespace'].append(true_map)
+                
+
+        kbar.update(i)
+        
+    mAP, mAR, F1_score = GetFullMetrics(predictions['prediction']['objects'],predictions['label']['objects'],range_min=5,range_max=100,IOU_threshold=0.5)
+    lines = [f'mAP: {mAP}\n', f'mAR: {mAR}\n', f'F1 score: {F1_score}\n']
+    with open(eval_path, 'w') as file:
+        file.writelines(lines)
+
+    mIoU = []
+    for i in range(len(predictions['prediction']['freespace'])):
+        # 0 to 124 means 0 to 50m
+        pred = predictions['prediction']['freespace'][i][:124].reshape(-1)>=0.5
+        label = predictions['label']['freespace'][i][:124].reshape(-1)
+        
+        intersection = np.abs(pred*label).sum()
+        union = np.sum(label) + np.sum(pred) -intersection
+        iou = intersection /union
+        mIoU.append(iou)
+
+    mIoU = np.asarray(mIoU).mean()
+    print('------- Freespace Scores ------------')
+    print('  mIoU',mIoU*100,'%')
+
+
 
 class FocalLoss(nn.Module):
     """
@@ -371,6 +428,7 @@ def perform_nms(valid_class_predictions, valid_box_predictions, nms_threshold):
         sorted_class_predictions = sorted_class_predictions[overlap_mask]
 
     return sorted_class_predictions, sorted_box_predictions
+
 def bbox_iou(box1, boxes):
 
     # currently inspected box
@@ -529,6 +587,10 @@ def GetFullMetrics(predictions,object_labels,range_min=5,range_max=100,IOU_thres
     print('------- Regression Errors------------')
     print('  Range Error:',np.mean(RangeError),'m')
     print('  Angle Error:',np.mean(AngleError),'degree')
+
+    mAP = np.mean(perfs['precision'])
+    mAR = np.mean(perfs['recall'])
+    return mAP, mAR, F1_score
 
 def GetDetMetrics(predictions,object_labels,threshold=0.2,range_min=5,range_max=70,IOU_threshold=0.2):
 
