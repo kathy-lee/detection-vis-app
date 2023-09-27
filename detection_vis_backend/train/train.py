e8e8dad41b867d429faeab972d3350e4b7722f8d
kathy <kangle.leehd@gmail.com>
2023-09-27 22:23:38 +0200

    Separate CreateDataloaders() func from train()

--- detection_vis_backend/train/train.py
+++ detection_vis_backend/train/train.py
@@ -18,7 +18,7 @@ from pathlib import Path
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim import lr_scheduler
-from torch.utils.data import ConcatDataset, DataLoader, random_split
+from torch.utils.data import ConcatDataset, DataLoader, random_split, Subset
 from tqdm import tqdm
 
 import sys
@@ -35,42 +35,138 @@ collate_func = {
     'RECORD': None,
 }    
 
-def train(datafiles: list, features: list, model_config: dict, train_config: dict, pretrained: str=None):
+def CreateDataLoaders(datafiles: list, features: list, model_config: dict, train_config: dict, use_original_split: bool, split_info_path: str):
     dataset_factory = DatasetFactory()
-    if train_config['dataloader']['splitmode'] == 'sequence':
-        assert len(datafiles) > 1
-        dataset_inst_list = []
-        for file in train_config['dataloader']['split_sequence']['train']:
-            dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
-            dataset_inst.prepare_for_train(features, train_config)
-            dataset_inst_list.append(dataset_inst)
-        train_dataset = ConcatDataset(dataset_inst_list)
-        dataset_inst_list = []
-        for file in train_config['dataloader']['split_sequence']['val']:
-            dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
-            dataset_inst.prepare_for_train(features, train_config)
-            dataset_inst_list.append(dataset_inst)
-        val_dataset = ConcatDataset(dataset_inst_list)
-        dataset_inst_list = []
-        for file in train_config['dataloader']['split_sequence']['test']:
-            dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
-            dataset_inst.prepare_for_train(features, train_config)
-            dataset_inst_list.append(dataset_inst)
-        test_dataset = ConcatDataset(dataset_inst_list)
+    if not use_original_split:
+        if train_config['dataloader']['splitmode'] == 'sequence':
+            assert len(datafiles) > 1
+            dataset_inst_list = []
+            for file in train_config['dataloader']['split_sequence']['train']:
+                dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            train_dataset = ConcatDataset(dataset_inst_list)
+            dataset_inst_list = []
+            for file in train_config['dataloader']['split_sequence']['val']:
+                dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            val_dataset = ConcatDataset(dataset_inst_list)
+            dataset_inst_list = []
+            for file in train_config['dataloader']['split_sequence']['test']:
+                dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            test_dataset = ConcatDataset(dataset_inst_list)
+            with open(split_info_path, 'w') as f:
+                f.write(f"TRAIN_SEQUENCES: {train_config['dataloader']['split_sequence']['train']}\n")
+                f.write(f"VAL_SEQUENCES: {train_config['dataloader']['split_sequence']['val']}\n")
+                f.write(f"TEST_SEQUENCES: {train_config['dataloader']['split_sequence']['test']}\n")
+        else:
+            dataset_inst_list = []
+            for file in datafiles:
+                dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            dataset = ConcatDataset(dataset_inst_list)
+            split = np.array(train_config['dataloader']['split_random'])
+            n_samples = len(dataset)
+            n_train = int(split[0] * n_samples)
+            n_val = int(split[1] * n_samples)
+            n_test = n_samples - n_train - n_val
+            train_dataset, val_dataset, test_dataset = random_split(dataset, [n_train, n_val,n_test], generator=torch.Generator().manual_seed(train_config['seed']))
+            train_ids = train_dataset.indices
+            val_ids = val_dataset.indices
+            test_ids = test_dataset.indices
+            with open(split_info_path, 'w') as f:
+                f.write(f"TRAIN_SAMPLE_IDS: {','.join(map(str, train_ids))}\n")
+                f.write(f"VAL_SAMPLE_IDS: {','.join(map(str, val_ids))}\n")
+                f.write(f"TEST_SAMPLE_IDS: {','.join(map(str, test_ids))}\n")
     else:
-        dataset_inst_list = []
-        for file in datafiles:
-            dataset_factory = DatasetFactory()
-            dataset_inst = dataset_factory.get_instance(file['parse'], file['id'])
-            dataset.prepare_for_train(features, train_config)
-            dataset_inst_list.append(dataset_inst)
-        dataset = ConcatDataset(dataset_inst_list)
-        split = np.array(train_config['dataloader']['split_random'])
-        n_samples = len(dataset)
-        n_train = int(split[0] * n_samples)
-        n_val = int(split[1] * n_samples)
-        n_test = n_samples - n_train - n_val
-        train_dataset, val_dataset, test_dataset = random_split(dataset, [n_train, n_val,n_test], generator=torch.Generator().manual_seed(seed))
+        dataset_type = datafiles[0]["parse"]
+        if dataset_type == "RADIal":
+            Sequences = {'val':['RECORD@2020-11-22_12.49.56', 'RECORD@2020-11-22_12.11.49',
+                                       'RECORD@2020-11-22_12.28.47','RECORD@2020-11-21_14.25.06'],
+                        'test':['RECORD@2020-11-22_12.45.05','RECORD@2020-11-22_12.25.47',
+                                'RECORD@2020-11-22_12.03.47','RECORD@2020-11-22_12.54.38']}
+            dataset = dataset_factory.get_instance(dataset_type, datafiles[0]['id'])
+            dataset.prepare_for_train(features, train_config, model_config)
+            labels = dataset.labels
+            dict_index_to_keys = {s:i for i,s in enumerate(dataset.sample_keys)}
+
+            Val_indexes = []
+            for seq in Sequences['val']:
+                idx = np.where(labels[:,14]==seq)[0]
+                Val_indexes.append(labels[idx,0])
+            Val_indexes = np.unique(np.concatenate(Val_indexes))
+
+            Test_indexes = []
+            for seq in Sequences['test']:
+                idx = np.where(labels[:,14]==seq)[0]
+                Test_indexes.append(labels[idx,0])
+            Test_indexes = np.unique(np.concatenate(Test_indexes))
+
+            val_ids = [dict_index_to_keys[k] for k in Val_indexes]
+            test_ids = [dict_index_to_keys[k] for k in Test_indexes]
+            train_ids = np.setdiff1d(np.arange(len(dataset)),np.concatenate([val_ids,test_ids]))  
+            train_dataset = Subset(dataset,train_ids)
+            val_dataset = Subset(dataset,val_ids)
+            test_dataset = Subset(dataset,test_ids)
+            with open(split_info_path, 'w') as f:
+                f.write(f"TRAIN_SAMPLE_IDS: {','.join(map(str, train_ids))}\n")
+                f.write(f"VAL_SAMPLE_IDS: {','.join(map(str, val_ids))}\n")
+                f.write(f"TEST_SAMPLE_IDS: {','.join(map(str, test_ids))}\n")
+        elif dataset_type == "CARRADA":
+            Sequences = {'train': ['2019-09-16-12-52-12', '2019-09-16-12-55-51', '2019-09-16-12-58-42', '2019-09-16-13-03-38', 
+                                   '2019-09-16-13-11-12', '2019-09-16-13-14-29', '2019-09-16-13-20-20', '2019-09-16-13-25-35', 
+                                   '2020-02-28-12-12-16', '2020-02-28-12-16-05', '2020-02-28-12-22-05', '2020-02-28-13-05-44', 
+                                   '2020-02-28-13-06-53', '2020-02-28-13-09-58', '2020-02-28-13-10-51', '2020-02-28-13-12-42', 
+                                   '2020-02-28-13-13-43', '2020-02-28-13-15-36'],
+                        'val': ['2019-09-16-13-06-41', '2019-09-16-13-23-22', '2020-02-28-12-17-57', '2020-02-28-12-20-22', 
+                               '2020-02-28-13-07-38', '2020-02-28-13-11-45'],
+                        'test': ['2019-09-16-13-13-01', '2019-09-16-13-18-33', '2020-02-28-12-13-54', '2020-02-28-12-23-30', 
+                                 '2020-02-28-13-08-51', '2020-02-28-13-14-35']}
+            file_ids = {'train': [], 'val': [], 'test': []}
+            dataset_inst_list = []
+            for id in file_ids['train']:
+                dataset_inst = dataset_factory.get_instance(dataset_type, id)
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            train_dataset = ConcatDataset(dataset_inst_list)
+            dataset_inst_list = []
+            for file in file_ids['val']:
+                dataset_inst = dataset_factory.get_instance(dataset_type, file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            val_dataset = ConcatDataset(dataset_inst_list)
+            dataset_inst_list = []
+            for file in file_ids['test']:
+                dataset_inst = dataset_factory.get_instance(dataset_type, file['id'])
+                dataset_inst.prepare_for_train(features, train_config, model_config)
+                dataset_inst_list.append(dataset_inst)
+            test_dataset = ConcatDataset(dataset_inst_list)
+            with open(split_info_path, 'w') as f:
+                f.write(f"TRAIN_SEQUENCES: {Sequences['train']}\n")
+                f.write(f"VAL_SEQUENCES: {Sequences['val']}\n")
+                f.write(f"TEST_SEQUENCES: {Sequences['test']}\n")
+        elif dataset_type == "RADDetDataset":
+            dataset = dataset_factory.get_instance(dataset_type, datafiles[0]['id'])
+            dataset.prepare_for_train(features, train_config, model_config)
+            sample_ids = [os.path.splitext(os.path.basename(f))[0] for f in dataset.anno_filenames]
+            with open(os.path.join(dataset.root_path, "original_split_info.json"), "r") as f:
+                dataset.original_split = json.load(f)
+            val_ids = [sample_ids.index(i) for i in dataset.original_split['val'] if i in sample_ids]
+            test_ids = [sample_ids.index(i) for i in dataset.original_split['test'] if i in sample_ids]
+            train_ids = np.setdiff1d(np.arange(len(dataset)), np.concatenate([val_ids,test_ids]))  
+            train_dataset = Subset(dataset,train_ids)
+            val_dataset = Subset(dataset,val_ids)
+            test_dataset = Subset(dataset,test_ids)
+            with open(split_info_path, 'w') as f:
+                f.write(f"TRAIN_SAMPLE_IDS: {','.join(map(str, train_ids))}\n")
+                f.write(f"VAL_SAMPLE_IDS: {','.join(map(str, val_ids))}\n")
+                f.write(f"TEST_SAMPLE_IDS: {','.join(map(str, test_ids))}\n")
+        else:
+            raise ValueError(f"{datafiles[0]['parse']} Dataset doesn't have default data split.")
     
     train_loader = DataLoader(train_dataset, 
                         batch_size=train_config['dataloader']['train']['batch_size'], 
@@ -90,7 +186,15 @@ def train(datafiles: list, features: list, model_config: dict, train_config: dic
                         num_workers=train_config['dataloader']['test']['num_workers'],
                         pin_memory=True,
                         collate_fn=collate_func[model_config['class']])
+    print('===========  Dataset  ==================:')
+    print('      Mode:', train_config['dataloader']['splitmode'])
+    print('      Training:', len(train_dataset))
+    print('      Validation:', len(val_dataset))
+    print('      Test:', len(test_dataset))
+    return train_loader, val_loader, test_loader
 
+
+def train(datafiles: list, features: list, model_config: dict, train_config: dict, use_original_split: bool, pretrained: str=None):    
     # Setup random seed
     torch.manual_seed(train_config['seed'])
     np.random.seed(train_config['seed'])
@@ -99,12 +203,14 @@ def train(datafiles: list, features: list, model_config: dict, train_config: dic
 
     # create experiment model name
     curr_date = datetime.now()
-    exp_name = model_config['type'] + '___' + curr_date.strftime('%b-%d-%Y___%H:%M:%S')
+    exp_name = model_config['class'] + '___' + curr_date.strftime('%b-%d-%Y___%H:%M:%S')
     print(exp_name)
 
     # save model path(also model name)
     with open("exp_info.txt", 'w') as f:
         f.write(exp_name)
+
+    split_info_path = os.path.join(output_folder, exp_name, 'samples_split.txt')
 
     # Initialize tensorboard
     output_folder = Path(os.getenv('MODEL_ROOTDIR'))
@@ -112,23 +218,12 @@ def train(datafiles: list, features: list, model_config: dict, train_config: dic
     (output_folder / exp_name).mkdir(parents=True, exist_ok=True)
     writer = SummaryWriter(output_folder / exp_name)
 
+    train_loader, val_loader, test_loader = CreateDataLoaders(datafiles, features, model_config, train_config, use_original_split, split_info_path)
+
     # save model lineage info
     train_info_path = os.path.join(output_folder, exp_name, 'train_info.txt')
     with open(train_info_path, 'w') as f:
         json.dump({"datafiles": datafiles, "features": features, "model_config": model_config, "train_config": train_config}, f)
-    # # read model lineage info
-    # with open('data.json', 'r') as file:
-    #     data = json.load(file)
-    #     A_from_file = data["list_of_dicts"]
-    #     B_from_file = data["single_dict"]
-
-
-    # save sample split info
-    split_info_path = os.path.join(output_folder, exp_name, 'samples_split.txt')
-    with open(split_info_path, 'w') as f:
-        f.write(f"TRAIN_SAMPLE_IDS: {','.join(map(str, train_ids))}\n")
-        f.write(f"VAL_SAMPLE_IDS: {','.join(map(str, val_ids))}\n")
-        f.write(f"TEST_SAMPLE_IDS: {','.join(map(str, test_ids))}\n")
 
     # save the evaluation of val dataset and test dataset
     val_eval_path = os.path.join(output_folder, exp_name, "val_eval.csv")
@@ -196,7 +291,7 @@ def train(datafiles: list, features: list, model_config: dict, train_config: dic
             if model_type == "FFTRadNet":
                 inputs = data[0].to(device).float()
                 label_map = data[1].to(device).float()
-                if(model_config['segmentation_head']=='True'):
+                if model_config['segmentation_head']:
                     seg_map_label = data[2].to(device).double()
             elif model_type == "RODNet":
                 inputs = data['radar_data'].to(device).float()
@@ -267,8 +362,7 @@ def train(datafiles: list, features: list, model_config: dict, train_config: dic
                                     segmentation_loss=freespace_loss, losses_params=train_config['losses'],
                                     device=device)
         elif model_type == "RODNet":
-            eval = RODNet_evaluation(net, val_loader, integrated_dataset.data_root, os.path.join(output_folder, exp_name), 
-                                     train_config, model_config, integrated_dataset.sensor_cfg['radar_cfg'], device)
+            eval = RODNet_evaluation(net, val_loader, os.path.join(output_folder, exp_name), train_config, model_config, device)
             
         history['val_loss'].append(eval['loss'])
         history['mAP'].append(eval['mAP'])
