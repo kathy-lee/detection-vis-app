71ca43e2e566b824d18607b75462a4fd497668b6
kathy <kangle.leehd@gmail.com>
2023-10-13 09:39:50 +0200

    add DAROD into model list

--- detection_vis_ui/pages/3_Train_model.py
+++ detection_vis_ui/pages/3_Train_model.py
@@ -177,7 +177,7 @@ def find_default_configs(dataset, feature, model):
 
 
 # Optinon 1: Train from scratch
-model_zoo = ["Choose a model type", "FFTRadNet", "RODNet", "RECORD", "RECORDNoLstm", "RECORDNoLstmMulti", "MVRECORD", "RADDet"]   # This will be changed when streamlit support selectbox with None as default option
+model_zoo = ["Choose a model type", "FFTRadNet", "RODNet", "RECORD", "RECORDNoLstm", "RECORDNoLstmMulti", "MVRECORD", "RADDet", "DAROD"]   # This will be changed when streamlit support selectbox with None as default option
 
 if train_mode == train_modes[0]:
   model = st.selectbox("Choose a model type:", model_zoo, index=0)
