69d5de9b150f910e8bc49b99752aebda690a983d
kathy <kangle.leehd@gmail.com>
2023-10-31 10:45:55 +0100

    add method to get datafile by name

--- data/crud.py
+++ data/crud.py
@@ -21,6 +21,9 @@ def get_datasetfiles(db: Session, dataset_id: int):
 def get_datafile(db: Session, file_id: int):
     return db.query(models.File).filter(models.File.id == file_id).first()
 
+def get_datafile_by_name(db: Session, file_name: str):
+    return db.query(models.File).filter(models.File.name == file_name).first()
+
 def add_model(db: Session, mlmodel: schemas.MLModelCreate):
     #model = models.MLmodel(name=mlmodel.name, description=mlmodel.description, flow_run_id=mlmodel.flow_run_id, flow_name=mlmodel.flow_name, parent_id=mlmodel.parent_id)  
     model = models.MLmodel(name=mlmodel.name, description=mlmodel.description, parent_id=mlmodel.parent_id)  
