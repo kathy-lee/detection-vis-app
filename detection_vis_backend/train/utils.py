f5dbe09144acb070cd82a21805a68e80d7ad71de
kathy <kangle.leehd@gmail.com>
2023-12-05 10:57:30 +0100

    adapt to the data dict format in FFTRadNet_collate()

--- detection_vis_backend/train/utils.py
+++ detection_vis_backend/train/utils.py
@@ -16,19 +16,32 @@ from shapely.geometry import Polygon
 from typing import Optional
 
 
-def FFTRadNet_collate(batch):
-    images = []
-    FFTs = []
-    segmaps = []
-    labels = []
-    encoded_label = []
+# def FFTRadNet_collate(batch):
+#     images = []
+#     FFTs = []
+#     segmaps = []
+#     labels = []
+#     encoded_label = []
 
-    for radar_FFT, segmap, out_label, box_labels in batch:
-        FFTs.append(torch.tensor(radar_FFT))
-        segmaps.append(torch.tensor(segmap))
-        encoded_label.append(torch.tensor(out_label))
-        labels.append(torch.from_numpy(box_labels))    
-    return torch.stack(FFTs), torch.stack(encoded_label),torch.stack(segmaps),labels
+#     for radar_FFT, segmap, out_label, box_labels in batch:
+#         FFTs.append(torch.tensor(radar_FFT))
+#         segmaps.append(torch.tensor(segmap))
+#         encoded_label.append(torch.tensor(out_label))
+#         labels.append(torch.from_numpy(box_labels))    
+#     return torch.stack(FFTs), torch.stack(encoded_label),torch.stack(segmaps),labels
+
+def FFTRadNet_collate(batch):
+    rd = [torch.tensor(item['RD']) for item in batch]
+    rd = torch.stack(rd, 0)
+    encoded_label = [torch.tensor(item['encoded_label']) for item in batch]
+    encoded_label = torch.stack(encoded_label, 0)
+    box_label = [torch.from_numpy(item['box_label']) for item in batch]
+    if 'seg_label' in batch[0]:
+        seg_label = [torch.tensor(item['seg_label']) for item in batch]
+        seg_label = torch.stack(seg_label, 0)
+        return {'RD': rd, 'encoded_label': encoded_label, 'box_label': box_label, 'seg_label': seg_label}
+    else:
+        return {'RD': rd, 'encoded_label': encoded_label, 'box_label': box_label}
 
 def DAROD_collate(batch):
     # raw labels from CARRADA dataset: 1,2,3
@@ -134,80 +147,6 @@ def decode(map,threshold):
             coordinates.append([R,A,C])
        
         return coordinates
-
-
-class FocalLoss(nn.Module):
-    """
-    Focal loss class. Stabilize training by reducing the weight of easily classified background sample and focussing
-    on difficult foreground detections.
-    """
-
-    def __init__(self, gamma=0, size_average=False):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.size_average = size_average
-
-    def forward(self, prediction, target):
-
-        # get class probability
-        pt = torch.where(target == 1.0, prediction, 1-prediction)
-
-        # compute focal loss
-        loss = -1 * (1-pt)**self.gamma * torch.log(pt+1e-6)
-
-        if self.size_average:
-            return loss.mean()
-        else:
-            return loss.sum()
-
-
-
-def pixor_loss(batch_predictions, batch_labels,param):
-
-    #########################
-    #  classification loss  #
-    #########################
-    classification_prediction = batch_predictions[:, 0,:, :].contiguous().flatten()
-    classification_label = batch_labels[:, 0,:, :].contiguous().flatten()
-
-    if(param['classification']=='FocalLoss'):
-        focal_loss = FocalLoss(gamma=2)
-        classification_loss = focal_loss(classification_prediction, classification_label)
-    else:
-        classification_loss = F.binary_cross_entropy(classification_prediction.double(), classification_label.double(),reduction='sum')
-
-    
-    #####################
-    #  Regression loss  #
-    #####################
-
-    regression_prediction = batch_predictions.permute([0, 2, 3, 1])[:, :, :, :-1]
-    regression_prediction = regression_prediction.contiguous().view([regression_prediction.size(0)*
-                        regression_prediction.size(1)*regression_prediction.size(2), regression_prediction.size(3)])
-    regression_label = batch_labels.permute([0, 2, 3, 1])[:, :, :, :-1]
-    regression_label = regression_label.contiguous().view([regression_label.size(0)*regression_label.size(1)*
-                                                           regression_label.size(2), regression_label.size(3)])
-
-    positive_mask = torch.nonzero(torch.sum(torch.abs(regression_label), dim=1))
-    pos_regression_label = regression_label[positive_mask.squeeze(), :]
-    pos_regression_prediction = regression_prediction[positive_mask.squeeze(), :]
-
-
-    T = batch_labels[:,1:]
-    P = batch_predictions[:,1:]
-    M = batch_labels[:,0].unsqueeze(1)
-
-    if(param['regression']=='SmoothL1Loss'):
-        reg_loss_fct = nn.SmoothL1Loss(reduction='sum')
-    else:
-        reg_loss_fct = nn.L1Loss(reduction='sum')
-    
-    regression_loss = reg_loss_fct(P*M,T)
-    NbPts = M.sum()
-    if(NbPts>0):
-        regression_loss/=NbPts
-
-    return classification_loss,regression_loss
 
 
 def RA_to_cartesian_box(data):
